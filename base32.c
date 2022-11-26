12e05f085177ee4d0c6d69517a5ccbf0cce94709
Thomas Egerer <thomas.egerer@port22.eu>
2022-11-26 16:27:55 +0100

    Use heap arrays for input/output
    Signed-off-by: Thomas Egerer <thomas.egerer@port22.eu>

--- base32.c
+++ base32.c
@@ -1,10 +1,17 @@
+#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
+#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-#ifndef __UNUSED
-# define __UNUSED __attribute__((unused))
-#endif
+#define __UNUSED __attribute__((unused))
+#define __PACKED __attribute__((packed))
+#define __CLEANUP(f) __attribute__((cleanup(f)))
+
+#define OUTPUT_WIDTH 76
+#define PADDING '='
+#define CONV_ARRAY_SIZE (sizeof(b32_convs) / sizeof(b32_convs[0]))
 
 /*
  * |       |               0               |               1           ...
@@ -38,7 +45,7 @@ struct baseXX_conversion_t {
 	unsigned char offset[2];
 	char mask[2];
 	char shift[2];
-}  __attribute__((packed)) b32_convs[8] = {
+}  __PACKED b32_convs[8] = {
 	{	/* 0 */
 		.offset = { 0, 0 },
 		.mask =   { 0xf8, 0x00 },
@@ -82,20 +89,37 @@ struct baseXX_conversion_t {
 	},
 };
 
-char alphabeth[32] = {
+char alphabeth[33] = {
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
 	'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5',
-	'6', '7',
+	'6', '7', PADDING,
 };
 
-static inline char byte_to_base32(const char *ptr, baseXX_conversion_t *b32_conv)
+static void cleanup_free(char **ptr)
+{
+	free(*ptr);
+}
+
+static inline char byte_to_base32(const char *ptr, size_t len, size_t idx,
+		baseXX_conversion_t *b32_conv)
 {
 	char c[2] = { 0 };
 	size_t i;
 
 	for (i = 0; i < 2; ++i)
 	{
-		c[i] |= (ptr[b32_conv->offset[i]] & b32_conv->mask[i]);
+		size_t offset = idx + b32_conv->offset[i];
+		if (offset >= len)
+		{
+			if (!i)
+			{	/* end of input, add padding sign '=' */
+				c[0] = 32;
+			}
+			break;
+		}
+
+		c[i] |= (ptr[offset] & b32_conv->mask[i]);
+
 		if (b32_conv->shift[i] < 0)
 		{
 			c[i] >>= -b32_conv->shift[i];
@@ -109,103 +133,125 @@ static inline char byte_to_base32(const char *ptr, baseXX_conversion_t *b32_conv
 	return alphabeth[c[0] | c[1]];
 }
 
-static void str_to_base32(const char *ptr, size_t len)
+static char *alloc_output_buffer_to_base32(size_t len)
 {
-	char buf[5];
-	size_t i, j, idx, end;
+	char *dst = NULL;
 
-	j = idx = 0;
-	end = (len / 5) * 5;
+	len = ((len + 4) / 5) * 8;
+	len += len / OUTPUT_WIDTH + 1;
 
-	while (idx < end)
+	dst = malloc(len);
+	dst[len - 1] = '\0';
+
+	return dst;
+}
+
+static char *str_to_base32(const char *ptr, size_t len)
+{
+	char *dst;
+	size_t i, idx_in, idx_out, end;
+
+	dst = alloc_output_buffer_to_base32(len);
+	idx_in = idx_out = 0;
+	end = ((len  + 4) / 5) * 5;
+
+	while (idx_in < end)
 	{
-		for (i = 0; i < sizeof(b32_convs) / sizeof(b32_convs[0]); ++i)
+		for (i = 0; i < CONV_ARRAY_SIZE; ++i)
 		{
-			fprintf(stdout, "%c%s", byte_to_base32(&ptr[idx], &b32_convs[i]),
-					!(++j % 76) ? "\n": "");
+			dst[idx_out++] = byte_to_base32(ptr, len, idx_in, &b32_convs[i]);
+			if ((idx_out % (OUTPUT_WIDTH + 1)) == (OUTPUT_WIDTH))
+			{
+				dst[idx_out++] = '\n';
+			}
 		}
-		idx += 5;
+		idx_in += 5;
 	}
 
-	if (len - end)
-	{
-		memcpy(buf, &ptr[end], len - end);
-		memset(&buf[len - end], 0, 5 - len + end);
-		end = (((len - end) * 8 + 4)/ 5);
-
-		for (i = 0; i < sizeof(b32_convs) / sizeof(b32_convs[0]); ++i)
-		{
-			fprintf(stdout, "%c%s", (i < end) ?
-					byte_to_base32(buf, &b32_convs[i]) : '=',
-					!(++j % 76) ? "\n": "");
-		}
-	}
+	return dst;
 }
 
 static size_t byte_from_base32(char c, baseXX_conversion_t *b32_conv,
-		char *dst, size_t *dst_idx)
+		char *dst, size_t *dst_idx, bool *printable)
 {
-	int i;
-	char cc;
+	size_t i;
+	int ret = 0;
+	char cc, diff = 0;
 
 	if (c >= 'A' && c <= 'Z')
 	{	/* values 0x00 - 0x */
-		c -= 'A';
+		diff = 'A';
 	}
 	else if (c >= '2' && c <= '7')
 	{	/* - 1 because zero and one are missing */
-		c -= 'Z' - 'A' - 1;
+		diff = 'Z' - 'A' - 1;
 	}
-	else
+	else if ('\n' != c && '\r' != c && PADDING != c)
 	{
-		return 0;
+		fprintf(stderr, "invalid input character 0x%02hhx\n", c);
 	}
 
-	for (i = 0; i < 2; ++i)
+	if (diff)
 	{
-		if (!b32_conv->mask[i])
+		c -= diff;
+
+		for (i = 0; i < 2; ++i)
 		{
-			continue;
+			if (!b32_conv->mask[i])
+			{
+				continue;
+			}
+
+			if (b32_conv->shift[i] < 0)
+			{
+				cc = c << -b32_conv->shift[i];
+			}
+			else if (b32_conv->shift[i] > 0)
+			{
+				cc = c >> b32_conv->shift[i];
+			}
+			else
+			{
+				cc = c;
+			}
+
+			dst[*dst_idx + i] |= (cc & b32_conv->mask[i]);
 		}
 
-		if (b32_conv->shift[i] < 0)
+		i = b32_conv->offset[1] - b32_conv->offset[0];
+
+		if (printable && i)
 		{
-			cc = c << -b32_conv->shift[i];
-		}
-		else if (b32_conv->shift[i] > 0)
-		{
-			cc = c >> b32_conv->shift[i];
-		}
-		else
-		{
-			cc = c;
+			if (*printable && !isprint(dst[*dst_idx]))
+			{
+				*printable = false;
+			}
 		}
 
-		dst[*dst_idx + i] |= (cc & b32_conv->mask[i]);
+		*dst_idx += i;
+		ret = 1;
 	}
 
-	*dst_idx += b32_conv->offset[1] - b32_conv->offset[0];
-	return 1;
+	return ret;
 }
 
-char *base32_to_str(const char *ptr, size_t len, size_t *ptr_len)
+char *base32_to_str(const char *ptr, size_t len, size_t *ptr_len,
+	bool *printable)
 {
 	char c;
-	size_t i, inc, idx, dst_idx;
-	char *tmp, *dst = NULL;
+	const char *tmp;
+	char *dst = NULL;
+	size_t i, inc, idx, dst_idx, dst_len;
 
 	if (len)
 	{
-		tmp = strchr(ptr, '=');
+		tmp = strchr(ptr, '=') ?: ptr + len;
 
-		if (tmp)
-		{	/* drop padding */
-			len = tmp - ptr;
-		}
-
-		if (len)
+		if (tmp - ptr)
 		{
-			dst = malloc(len);
+			dst_len = ((tmp - ptr) + 7) / 8 * 5 + 1;
+			// TODO Idkw, but if I use malloc here, valgrind complains...
+			dst = calloc(dst_len, 1);
 		}
 	}
 
@@ -214,8 +260,8 @@ char *base32_to_str(const char *ptr, size_t len, size_t *ptr_len)
 	while (idx < len)
 	{
 		c = ptr[idx];
-		inc = byte_from_base32(c, &b32_convs[i], dst, &dst_idx);
-		i = (i + inc) % (sizeof(b32_convs) / sizeof(b32_convs[0]));
+		inc = byte_from_base32(c, &b32_convs[i], dst, &dst_idx, printable);
+		i = (i + inc) % CONV_ARRAY_SIZE;
 		++idx;
 	}
 
@@ -224,24 +270,39 @@ char *base32_to_str(const char *ptr, size_t len, size_t *ptr_len)
 		*ptr_len = dst_idx;
 	}
 
+	// TODO: see above comment about valgrind
+	//if (dst)
+	//{
+	//	memset(&dst[dst_idx], 0, dst_len - dst_idx);
+	//}
+
 	return dst;
 }
 
 int main(int argc __UNUSED, char *argv[] __UNUSED)
 {
-	char *buf;
 	size_t len;
+	bool printable = true;
+	char *input, *b32 __CLEANUP(cleanup_free) = NULL,
+		 *buf __CLEANUP(cleanup_free) = NULL;
 
-	buf = "bin  data  Desktop  Documents  Downloads  GNUstep  i  Mail  mbox  "
+	input = "bin  data  Desktop  Documents  Downloads  GNUstep  i  Mail  mbox  "
 		"share  tmp  tmux.conf  usr  var";
 
-	str_to_base32(buf, strlen((char *)buf));
+	b32 = str_to_base32(input, strlen(input));
+	fprintf(stdout, "%s\n", b32);
+	buf = base32_to_str(b32, strlen(b32), &len, &printable);
+	if (printable)
+	{
+		fprintf(stdout, "%s\n", buf);
+	}
+	else
+	{
+		fprintf(stdout, "output contains unprintable characters\n", buf);
+	}
 
-	buf = base32_to_str("MJUW4IBAMRQXIYJAEBCGK43LORXXAIBAIRXWG5LNMVXHI4ZAEBCG653ONRXWCZDTEAQEOTSVON2G"
-			"K4BAEBUSAICNMFUWYIBANVRG66BAEBZWQYLSMUQCA5DNOAQCA5DNOV4C4Y3PNZTCAIDVONZCAIDW"
-			"MFZA====", 2 * 76 + 8, &len);
-	fprintf(stdout, "\n\"%s\" (%zu)\n", buf, len);
-	free(buf);
+	assert(len == strlen(input));
+	assert(!memcmp(buf, input, len));
 
 	return 0;
 }
