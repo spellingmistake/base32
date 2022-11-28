ae676f892d982e389eaf4ac968b819e3dd12dc07
Thomas Egerer <thomas.egerer@port22.eu>
2022-11-28 20:40:20 +0100

    Add test vectors from RFC 4648
    Signed-off-by: Thomas Egerer <thomas.egerer@port22.eu>

--- base_test.c
+++ base_test.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "base32.h"
 #include "base64.h"
@@ -12,65 +13,84 @@ static void cleanup_free(char **ptr)
 	free(*ptr);
 }
 
-static void test_base32(const char *input)
+typedef struct test_vectors_t test_vectors_t;
+
+struct test_vectors_t {
+	const char *input;
+	const char *expected;
+	bool printable;
+};
+
+/* test vectors from RFC 4648 */
+static test_vectors_t tv_base64[] = {
+	{ "", "", true },
+	{ "f", "Zg==", true },
+	{ "fo", "Zm8=", true },
+	{ "foo", "Zm9v", true },
+	{ "foob", "Zm9vYg==", true },
+	{ "fooba", "Zm9vYmE=", true },
+	{ "foobar", "Zm9vYmFy", true },
+};
+
+static test_vectors_t tv_base32[] = {
+	{ "", "", true },
+	{ "f", "MY======", true },
+	{ "fo", "MZXQ====", true },
+	{ "foo", "MZXW6===", true },
+	{ "foob", "MZXW6YQ=", true },
+	{ "fooba", "MZXW6YTB", true },
+	{ "foobar", "MZXW6YTBOI======", true },
+};
+
+static void test_baseXX(test_vectors_t *tvs, size_t i, const char *base,
+		bool verbose, char *(*str_to_baseXX)(const char *, size_t len),
+		char *(*baseXX_to_str)(const char *, size_t, size_t *, bool *))
 {
 	size_t len;
 	bool printable = true;
-	char *b32 __CLEANUP(cleanup_free) = NULL,
+	char *bXX __CLEANUP(cleanup_free) = NULL,
 		 *buf __CLEANUP(cleanup_free) = NULL;
 
-	b32 = str_to_base32(input, strlen(input));
-	fprintf(stdout, "%s\n", b32);
-	buf = base32_to_str(b32, strlen(b32), &len, &printable);
-	if (printable)
+	bXX = str_to_baseXX(tvs[i].input, strlen(tvs[i].input));
+	buf = baseXX_to_str(bXX, strlen(bXX), &len, &printable);
+
+	assert(tvs[i].printable == printable);
+	assert(len == strlen(tvs[i].input));
+	assert(!memcmp(buf, tvs[i].input, len));
+	fprintf(stdout, "%s test vector %zu ok", base, i + 1);
+	if (verbose)
 	{
-		fprintf(stdout, "%s\n", buf);
+		fprintf(stdout, " (input '%s')\n", tvs[i].input);
 	}
 	else
 	{
-		fprintf(stdout, "output contains unprintable characters\n");
+		fprintf(stdout, "\n");
 	}
-
-	assert(len == strlen(input));
-	assert(!memcmp(buf, input, len));
 }
 
-static void test_base64(const char *input)
+static void test_base64(void)
 {
-	size_t len;
-	bool printable = true;
-	char *b64 __CLEANUP(cleanup_free) = NULL,
-		 *buf __CLEANUP(cleanup_free) = NULL;
+	size_t i;
 
-	b64 = str_to_base64(input, strlen(input));
-	fprintf(stdout, "%s\n", b64);
-	buf = base64_to_str(b64, strlen(b64), &len, &printable);
-	if (printable)
+	for (i = 0; i < sizeof(tv_base64) / sizeof(tv_base64[0]); ++i)
 	{
-		fprintf(stdout, "%s\n", buf);
+		test_baseXX(tv_base64, i, __func__, false, str_to_base64, base64_to_str);
 	}
-	else
-	{
-		fprintf(stdout, "output contains unprintable characters\n");
-	}
+}
 
-	assert(len == strlen(input));
-	assert(!memcmp(buf, input, len));
+static void test_base32(void)
+{
+	size_t i;
+
+	for (i = 0; i < sizeof(tv_base32) / sizeof(tv_base32[0]); ++i)
+	{
+		test_baseXX(tv_base32, i, __func__, false, str_to_base32, base32_to_str);
+	}
 }
 
 int main(int argc __UNUSED, char *argv[] __UNUSED)
 {
-	char *input;
-
-	input = "bin  data  Desktop  Documents  Downloads  GNUstep  i  Mail  mbox  "
-		"share  tmp  tmux.conf  usr  var";
-
-	test_base32(input);
-	test_base32("12");
-	test_base32("123");
-	test_base64(input);
-	test_base64("12");
-	test_base64("123");
-
+	test_base64();
+	test_base32();
 	return 0;
 }
